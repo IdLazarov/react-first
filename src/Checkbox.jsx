57e5f8a365d01b68eb6b2b1ad19dcc4fc5718678
Ivan <arti244@abv.bg>
2020-06-25 19:13:04 +0300

    added checkboxes

---
+++ src/Checkbox.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+function Checkbox(){
+    return(
+        <div>
+            <p>Check this</p>
+            <input type="checkbox"></input>
+        </div>
+        
+
+    );
+}
+
+export default Checkbox
