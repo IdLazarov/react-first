f0ddad32a340bd174f85b074d6f25b8c92646e49
Ivan <arti244@abv.bg>
2020-07-06 17:07:19 +0300

    added line-through when item is completed

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,14 +3,19 @@ import React from "react"
 
 
 function TodoItem(props) {
+    let completedStyle = {
+        color: "#cdcdcd",
+        textDecoration: "line-through",
 
+    }
     return (
+        
         <div className="todo-item">
             <input type="checkbox"
                    checked={props.item.completed}
                    onChange={()=>{props.handleChange(props.item.id)}}
             ></input>
-            <p>{props.item.text}</p>
+            <p style={props.item.completed ? completedStyle : null}>{props.item.text}</p>
 
         </div>
 
